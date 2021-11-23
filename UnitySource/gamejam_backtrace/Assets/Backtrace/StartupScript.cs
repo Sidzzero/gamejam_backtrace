f04352dba8651bbf4f176c8b371d07d0880627e7
sidz_ripper <srikanth.siddhu@gmail.com>
2021-11-23 19:44:32 +0530

    initial files
    need to check gitignore

---
+++ UnitySource/gamejam_backtrace/Assets/Backtrace/StartupScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(5 / a);
+    }
+    int a = 0;
+   
+}
