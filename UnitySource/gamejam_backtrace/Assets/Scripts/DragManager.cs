5e3f24eb89de9f7f5bac1801e7ebe19aca208789
sidz_ripper <srikanth.siddhu@gmail.com>
2021-12-04 17:50:03 +0530

    Working on Drag System

---
+++ UnitySource/gamejam_backtrace/Assets/Scripts/DragManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace sidz.wogame
+{
+    public class DragManager : MonoBehaviour
+    {
+        //Holds all the draggable items 
+        //Test if if is in the region
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
