43172822d805afdcc132572dc9f6a1bc0ddbf29c
sidz_ripper <srikanth.siddhu@gmail.com>
2021-12-04 15:44:01 +0530

    Updated expcetion code

--- UnitySource/gamejam_backtrace/Assets/Backtrace/StartupScript.cs
+++ UnitySource/gamejam_backtrace/Assets/Backtrace/StartupScript.cs
@@ -7,9 +7,16 @@ public class StartupScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //  Debug.LogError("This is a Backtrace Error", this);
-        throw new System.Exception("This is a Backtrace Error");
+      //  throw new System.Exception(" 2This is a Backtrace Error");
+        try
+        {
+            throw new System.Exception("This is a Backtrace Error !:]");
+        }
+        catch (System.Exception exp)
+        {
+            Debug.LogError(exp, this);
+        }
+
     }
-   
-   
 }
+
